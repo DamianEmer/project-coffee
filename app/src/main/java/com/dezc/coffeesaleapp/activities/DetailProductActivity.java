d517137a5d3d4e700f2c236128f2d42ede4d774b
Damian <emer_smart17@outlook.com>
2019-04-24 12:52:11 -0500

    Adaptacion de imagen en vista detalle de producto

--- app/src/main/java/com/dezc/coffeesaleapp/activities/DetailProductActivity.java
+++ app/src/main/java/com/dezc/coffeesaleapp/activities/DetailProductActivity.java
@@ -2,14 +2,17 @@ package com.dezc.coffeesaleapp.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.dezc.coffeesaleapp.R;
 
 import org.w3c.dom.Text;
 
 public class DetailProductActivity extends AppCompatActivity {
 
+    private ImageView imageProduct;
     private TextView nameProduct;
     private TextView priceProduct;
 
@@ -31,11 +34,14 @@ public class DetailProductActivity extends AppCompatActivity {
     }
 
     private void setData(String idProduct, String contentProduct){
+
+        imageProduct = (ImageView) findViewById(R.id.imageProduct);
         nameProduct = (TextView) findViewById(R.id.nameTextView);
         priceProduct = (TextView) findViewById(R.id.priceTextView);
 
         nameProduct.setText(contentProduct);
         priceProduct.setText(idProduct);
+        Glide.with(this).load(R.drawable.coffee).into(imageProduct);
     }
 
 }
