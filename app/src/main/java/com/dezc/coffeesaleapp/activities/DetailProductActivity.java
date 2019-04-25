c4d2df4f59c29cf09a411a54d32bac14ffb539b4
Damian <emer_smart17@outlook.com>
2019-04-25 14:33:26 -0500

    Carrito de compras
    Se implementa carrito de compras y entra en función para agregar un producto a esta lista desde la vista detalle de producto

--- app/src/main/java/com/dezc/coffeesaleapp/activities/DetailProductActivity.java
+++ app/src/main/java/com/dezc/coffeesaleapp/activities/DetailProductActivity.java
@@ -2,19 +2,24 @@ package com.dezc.coffeesaleapp.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.dezc.coffeesaleapp.R;
-
-import org.w3c.dom.Text;
+import com.dezc.coffeesaleapp.fragments.WishFragment;
+import com.dezc.coffeesaleapp.fragments.dummy.DummyContent;
+import com.dezc.coffeesaleapp.models.Product;
 
 public class DetailProductActivity extends AppCompatActivity {
 
     private ImageView imageProduct;
     private TextView nameProduct;
     private TextView priceProduct;
+    private Button btnAddCart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,10 +43,18 @@ public class DetailProductActivity extends AppCompatActivity {
         imageProduct = (ImageView) findViewById(R.id.imageProduct);
         nameProduct = (TextView) findViewById(R.id.nameTextView);
         priceProduct = (TextView) findViewById(R.id.priceTextView);
+        btnAddCart = (Button) findViewById(R.id.button_addCart);
 
         nameProduct.setText(contentProduct);
         priceProduct.setText(idProduct);
         Glide.with(this).load(R.drawable.coffee).into(imageProduct);
+
+    }
+
+    public void addCart(View view){
+        Toast.makeText(this, "Agregado al carrito", Toast.LENGTH_LONG).show();
+        DummyContent.addItem(new Product("001","Producto nuevo", "x"));
+        WishFragment.myWishRecyclerViewAdapter.notifyItemInserted(DummyContent.ITEMS.size()+1);
     }
 
 }
