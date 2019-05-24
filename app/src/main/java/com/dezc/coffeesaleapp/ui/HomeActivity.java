0a9304ee1da36c098adde68997dd2518039bb63a
Julio Adán Pérez García <julioperezag@gmail.com>
2019-05-24 00:26:07 -0500

    Add menu to home

--- app/src/main/java/com/dezc/coffeesaleapp/ui/HomeActivity.java
+++ app/src/main/java/com/dezc/coffeesaleapp/ui/HomeActivity.java
@@ -1,11 +1,14 @@
 package com.dezc.coffeesaleapp.ui;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import com.dezc.coffeesaleapp.R;
 import com.dezc.coffeesaleapp.models.Product;
@@ -27,5 +30,16 @@ public class HomeActivity extends AppCompatActivity implements OnProductClickLis
     public void onProductClickListener(Product item) {
         mViewModel.getProduct().postValue(item);
         Navigation.findNavController(this, R.id.nav_host_home).navigate(R.id.action_homeFragment_to_detailProductFragment);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return NavigationUI.onNavDestinationSelected(item, Navigation.findNavController(this, R.id.nav_host_home)) || super.onOptionsItemSelected(item);
     }
 }
