2c42a425fdfde4073af59e5080043ac849da1004
Julio Adán Pérez García <julioperezag@gmail.com>
2019-06-02 03:04:32 -0500

    Add Data bound helper class to FirebaseRecyclerAdapter

---
+++ app/src/main/java/com/dezc/coffeesaleapp/ui/utils/commons/adapters/DataBoundFirebaseRecyclerAdapter.kt
@@ -0,0 +1,20 @@
+package com.dezc.coffeesaleapp.ui.utils.commons.adapters
+
+import android.view.ViewGroup
+import androidx.databinding.ViewDataBinding
+import com.firebase.ui.database.FirebaseRecyclerAdapter
+import com.firebase.ui.database.FirebaseRecyclerOptions
+
+abstract class DataBoundFirebaseRecyclerAdapter<T, V : ViewDataBinding>(options: FirebaseRecyclerOptions<T>) : FirebaseRecyclerAdapter<T, DataBoundViewHolder<V>>(options) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DataBoundViewHolder<V> = DataBoundViewHolder(createBinding(parent))
+
+    override fun onBindViewHolder(holder: DataBoundViewHolder<V>, position: Int, item: T) {
+        bind(holder.binding, position, item)
+        holder.binding.executePendingBindings()
+    }
+
+    abstract fun createBinding(parent: ViewGroup): V
+
+    abstract fun bind(binding: V, position: Int, item: T)
+}
