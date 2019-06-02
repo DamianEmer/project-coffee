15669829894b36464c138aa2a3b6cf400e53d2c1
Julio Adán Pérez García <julioperezag@gmail.com>
2019-06-02 03:11:37 -0500

    Add getStringArray function

--- app/src/main/java/com/dezc/coffeesaleapp/functions/ResourcesFunction.kt
+++ app/src/main/java/com/dezc/coffeesaleapp/functions/ResourcesFunction.kt
@@ -2,10 +2,19 @@ package com.dezc.coffeesaleapp.functions
 
 import android.app.Activity
 import android.content.res.Resources
+import androidx.annotation.ArrayRes
 import androidx.annotation.IntegerRes
+import androidx.fragment.app.Fragment
 
 fun Activity.getInteger(@IntegerRes idRes: Int, defaultInt: Int = 0): Int = try {
-    applicationContext.resources.getInteger(idRes)
+    resources.getInteger(idRes)
 } catch (e: Resources.NotFoundException) {
     defaultInt
+}
+
+fun Fragment.getStringArray(@ArrayRes idRes: Int,
+                            defaultStringArray: Array<String> = emptyArray()): Array<String> = try {
+    resources.getStringArray(idRes)
+} catch (e: Resources.NotFoundException) {
+    defaultStringArray
 }
