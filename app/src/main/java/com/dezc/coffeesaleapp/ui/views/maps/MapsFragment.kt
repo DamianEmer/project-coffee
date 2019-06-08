19c2cef08c57767a9640a314ec23d6a2260932ca
Damian <emer_smart17@outlook.com>
2019-06-08 16:48:31 -0500

    Add address form
    purely degin

--- app/src/main/java/com/dezc/coffeesaleapp/ui/views/maps/MapsFragment.kt
+++ app/src/main/java/com/dezc/coffeesaleapp/ui/views/maps/MapsFragment.kt
@@ -9,19 +9,8 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import com.dezc.coffeesaleapp.R
 import com.dezc.coffeesaleapp.databinding.FragmentMapsBinding
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.MapView
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
-import kotlinx.android.synthetic.main.fragment_maps.*
 
-class MapsFragment : Fragment(), OnMapReadyCallback {
-
-    private var mMap: GoogleMap? = null
-
-    private lateinit var mMapReference: MapView
+class MapsFragment : Fragment() {
 
     private lateinit var mBinding: FragmentMapsBinding
 
@@ -31,53 +20,11 @@ class MapsFragment : Fragment(), OnMapReadyCallback {
         return mBinding.root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        mMapReference = map
-        mMapReference.onCreate(savedInstanceState)
-        mMapReference.getMapAsync(this)
-    }
-
-    override fun onStart() {
-        super.onStart()
-        mMapReference.onStart()
-    }
-
-    override fun onResume() {
-        super.onResume()
-        mMapReference.onResume()
-    }
-
-    override fun onStop() {
-        super.onStop()
-        mMapReference.onStop()
-    }
-
-    override fun onLowMemory() {
-        super.onLowMemory()
-        mMapReference.onLowMemory()
-    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {    }
 
     fun onNext(view: View) {
         Log.d("PaymentFragment: ", "Siguiente sección (Tipo de pago)")
         Navigation.findNavController(view).navigate(R.id.action_mapsFragment_to_paymentFragment);
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
-
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap!!.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap!!.moveCamera(CameraUpdateFactory.newLatLng(sydney))
-    }
 }
