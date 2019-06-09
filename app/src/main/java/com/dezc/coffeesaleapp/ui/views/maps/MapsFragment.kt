e89f88eae22b829126ceed9a80a69442444c0cb3
Damian <emer_smart17@outlook.com>
2019-06-08 20:12:19 -0500

    Validation of editTextInputs in address form

--- app/src/main/java/com/dezc/coffeesaleapp/ui/views/maps/MapsFragment.kt
+++ app/src/main/java/com/dezc/coffeesaleapp/ui/views/maps/MapsFragment.kt
@@ -5,12 +5,20 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import com.dezc.coffeesaleapp.R
 import com.dezc.coffeesaleapp.databinding.FragmentMapsBinding
+import com.dezc.coffeesaleapp.functions.addEditTextValidators
+import com.dezc.coffeesaleapp.types.Validators
+import com.dezc.coffeesaleapp.types.codePostalValidator
+import kotlinx.android.synthetic.main.fragment_maps.*
+import kotlinx.android.synthetic.main.fragment_maps.view.*
 
 class MapsFragment : Fragment() {
+
+    private var codePostalValidators: Validators = arrayListOf(codePostalValidator())
 
     private lateinit var mBinding: FragmentMapsBinding
 
@@ -20,11 +28,26 @@ class MapsFragment : Fragment() {
         return mBinding.root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        text_input_edit_postal_code.addEditTextValidators(codePostalValidators, "Es requerido el CP")
+    }
 
     fun onNext(view: View) {
-        Log.d("PaymentFragment: ", "Siguiente sección (Tipo de pago)")
-        Navigation.findNavController(view).navigate(R.id.action_mapsFragment_to_paymentFragment);
+        if(text_input_edit_street.text.toString().isNotEmpty() &&
+                text_input_edit_suburb.text.toString().isNotEmpty() &&
+                text_input_edit_town.text.toString().isNotEmpty() &&
+                text_input_edit_city.text.toString().isNotEmpty() &&
+                text_input_edit_outdoor_number.text.toString().isNotEmpty() &&
+                text_input_edit_postal_code.text.toString().isNotEmpty()){
+            Navigation.findNavController(view).navigate(R.id.action_mapsFragment_to_paymentFragment)
+        }else{
+            Toast.makeText(context, "Ingrese sus datos correctamente", Toast.LENGTH_LONG).show()
+        }
+
+    }
+
+    fun onBack(view: View) {
+        Navigation.findNavController(view).navigate(R.id.action_mapsFragment_to_homeFragment)
     }
 
 }
