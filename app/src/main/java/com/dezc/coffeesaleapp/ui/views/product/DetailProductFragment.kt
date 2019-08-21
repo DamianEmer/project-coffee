29409016f1069e43972d7e778eb8e6c6346ad59e
Julio Adán Pérez García <julioperezag@gmail.com>
2019-08-21 18:25:09 -0500

    Add launcher icon

--- app/src/main/java/com/dezc/coffeesaleapp/ui/views/product/DetailProductFragment.kt
+++ app/src/main/java/com/dezc/coffeesaleapp/ui/views/product/DetailProductFragment.kt
@@ -2,7 +2,6 @@ package com.dezc.coffeesaleapp.ui.views.product
 
 import android.app.AlertDialog
 import android.app.Dialog
-import android.content.DialogInterface
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -10,29 +9,20 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.Navigation
 import com.bumptech.glide.Glide
 import com.dezc.coffeesaleapp.R
 import com.dezc.coffeesaleapp.databinding.FragmentDetailProductBinding
-import com.dezc.coffeesaleapp.functions.addEditTextValidators
-import com.dezc.coffeesaleapp.functions.getSupportFragmentManager
-import com.dezc.coffeesaleapp.functions.isNotEmpty
 import com.dezc.coffeesaleapp.models.Product
 import com.dezc.coffeesaleapp.models.ProductCart
-import com.dezc.coffeesaleapp.types.Validators
 import com.dezc.coffeesaleapp.viewmodels.ProductViewModel
 import com.dezc.coffeesaleapp.viewmodels.WishViewModel
-import com.dezc.coffeesaleapp.types.quantityValidator
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
-import kotlinx.android.synthetic.main.dialog_additional.*
 import kotlinx.android.synthetic.main.dialog_additional.view.*
-import kotlinx.android.synthetic.main.dialog_confirm.*
 import kotlinx.android.synthetic.main.dialog_confirm.view.*
-import kotlinx.android.synthetic.main.dialog_confirm.view.dialog_confirm
 import kotlinx.android.synthetic.main.fragment_detail_product.*
 
 class DetailProductFragment : Fragment() {
@@ -61,31 +51,31 @@ class DetailProductFragment : Fragment() {
     override fun onPause() {
         super.onPause()
         Log.i("DetailProductFragment", "----> onPauseActive")
-        mProductViewModel.productSelect.removeObserver {  }
+        mProductViewModel.productSelect.removeObserver { }
     }
 
     private fun setDataEditable(productCart: ProductCart) {
-        Log.i(DetailProductFragment::class.simpleName, "productCart: "+ productCart.idProduct)
+        Log.i(DetailProductFragment::class.simpleName, "productCart: " + productCart.idProduct)
         mBinding.productCart = productCart
     }
 
     private fun setData(product: Product) {
         mBinding.product = product
-        Glide.with(this).load(R.drawable.coffee).override(800,200).into(image_product)
+        Glide.with(this).load(R.drawable.coffee).override(800, 200).into(image_product)
     }
 
     fun addCart(view: View) {
-        if(this.quantityCount >= 1) {
+        if (this.quantityCount >= 1) {
             val priceTotal: Float = java.lang.Float.parseFloat((mBinding.product!!.price * this.quantityCount).toString())
             this.onDialogConfirm(view, priceTotal)
-        }else {
+        } else {
             Toast.makeText(view.context, "Ingresa una cantidad", Toast.LENGTH_LONG).show()
         }
     }
 
     var additionalNotes: String = "";
 
-    fun onCreateModal(view: View){
+    fun onCreateModal(view: View) {
         val inflater = layoutInflater;
         val dialogLayout = inflater.inflate(R.layout.dialog_additional, null);
         val builder = AlertDialog.Builder(context!!);
@@ -101,29 +91,29 @@ class DetailProductFragment : Fragment() {
 
     var quantityCount: Int = 0;
 
-    fun onIncrementCounter(view: View){
+    fun onIncrementCounter(view: View) {
         this.quantityCount++;
         this.text_layout_quantity.text = this.quantityCount.toString();
-        if(this.quantityCount > 0) this.btn_decrement.isClickable = true;
+        if (this.quantityCount > 0) this.btn_decrement.isClickable = true;
     }
 
     fun onDecrementCounter(view: View) {
-        if(this.quantityCount > 0) {
+        if (this.quantityCount > 0) {
             this.quantityCount--;
             this.text_layout_quantity.text = this.quantityCount.toString();
-        }else {
+        } else {
             this.btn_decrement.isClickable = false;
         }
     }
 
-    fun isExistQuantity(view: View, value: Int):Int {
-        return if(value != 0)
+    fun isExistQuantity(view: View, value: Int): Int {
+        return if (value != 0)
             value
         else
             0
     }
 
-    private fun onDialogConfirm(view: View, priceTotal: Float){
+    private fun onDialogConfirm(view: View, priceTotal: Float) {
         val builder: Dialog = Dialog(context!!)
         val inflater = layoutInflater
         val dialogLayout = inflater.inflate(R.layout.dialog_confirm, null)
