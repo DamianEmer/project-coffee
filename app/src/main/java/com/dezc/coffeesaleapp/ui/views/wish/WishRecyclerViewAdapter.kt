b3ab3eaa6f0fb3bc7c8e05344173eb0115b15ea3
Julio Adán Pérez García <julioperezag@gmail.com>
2019-08-23 02:32:27 -0500

    Fix firebase adapter

--- app/src/main/java/com/dezc/coffeesaleapp/ui/views/wish/WishRecyclerViewAdapter.kt
+++ app/src/main/java/com/dezc/coffeesaleapp/ui/views/wish/WishRecyclerViewAdapter.kt
@@ -5,27 +5,21 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import com.dezc.coffeesaleapp.R.layout.item_wish
 import com.dezc.coffeesaleapp.databinding.ItemWishBinding
-import com.dezc.coffeesaleapp.models.Product
 import com.dezc.coffeesaleapp.models.ProductCart
 import com.dezc.coffeesaleapp.ui.utils.callbacks.OnProductClickListener
-import com.dezc.coffeesaleapp.ui.utils.commons.adapters.DataBoundRecyclerAdapter
-import com.dezc.coffeesaleapp.ui.utils.commons.adapters.DataBoundViewHolder
-import com.firebase.ui.database.FirebaseRecyclerAdapter
+import com.dezc.coffeesaleapp.ui.utils.commons.adapters.DataBoundFirebaseRecyclerAdapter
 import com.firebase.ui.database.FirebaseRecyclerOptions
 
 class WishRecyclerViewAdapter(
         options: FirebaseRecyclerOptions<ProductCart>,
         private val mOnProductClickListener: OnProductClickListener?
-) : FirebaseRecyclerAdapter<ProductCart, DataBoundViewHolder<ItemWishBinding>>(options) {
+) : DataBoundFirebaseRecyclerAdapter<ProductCart, ItemWishBinding>(options) {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DataBoundViewHolder<ItemWishBinding> =
-        DataBoundViewHolder(DataBindingUtil.inflate(LayoutInflater.from(parent.context), item_wish, parent, false))
+    override fun createBinding(parent: ViewGroup): ItemWishBinding =
+            DataBindingUtil.inflate(LayoutInflater.from(parent.context), item_wish, parent, false)
 
-    override fun onBindViewHolder(holder: DataBoundViewHolder<ItemWishBinding>, position: Int, product: ProductCart) {
-        holder.binding.listener = mOnProductClickListener
-        holder.binding.product = product
+    override fun bind(binding: ItemWishBinding, item: ProductCart, position: Int) {
+        binding.listener = mOnProductClickListener
+        binding.product = item
     }
-
-
 }
-
